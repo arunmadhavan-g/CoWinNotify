415c2172aedf87ec5c6e054a18d1d30c868c702e
arunmadhavan-g <arunmadhavan.g@gmail.com>
2021-05-15 21:13:39 +0530

    Arun | modifies the fetch to use public url and remove unnecessary logging

--- index.js
+++ index.js
@@ -8,41 +8,22 @@ const today =  () => {
 
 
 const checkCowin = () => 
-	fetch('https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id=571&date='+today(), {
-		method: 'GET',
+	fetch('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=571&date='+today(), {
 		headers: {
-			"accept": "application/json, text/plain, */*",
-		    "accept-language": "en-IN,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
-		    "authorization": process.env.TOKEN,
-		    "cache-control": "no-cache",
-		    "pragma": "no-cache",
-		    "sec-ch-ua": "\" Not A;Brand\";v=\"99\", \"Chromium\";v=\"90\", \"Google Chrome\";v=\"90\"",
-		    "sec-ch-ua-mobile": "?0",
-		    "sec-fetch-dest": "empty",
-		    "sec-fetch-mode": "cors",
-		    "sec-fetch-site": "cross-site"
+			"user-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
 		},
-		referrer: "https://selfregistration.cowin.gov.in/",
-  		referrerPolicy: "strict-origin-when-cross-origin",
-  		"body": null,
-  		"mode": "cors"
-	})
-	.then( response => response.text())
+		"method": "GET"
+		}
+	 )
+	.then( response => response.json())
 
 const fetchAvailable = (data) => {
-	console.log(">>>>>>>>>>",data)
 	return data.centers.filter(x => x.sessions.some( y => y.min_age_limit === 18 && y.available_capacity > 0) )	
 }
 
 
 const keepFetching = () => checkCowin()
 				.then( data =>  new Promise((resolve, reject) => {
-					var json = {centers: []}
-					try{
-						json = JSON.parse(data)	
-					}catch(e){
-						reject(new Error("Error parsing JSON:" + e.message + data))
-					}
 					resolve(fetchAvailable(data))	
 				}))
 
@@ -79,13 +60,12 @@ const sendErrorMail  = (e) =>
 const notifyIfAny = () => 
 keepFetching().then(data => {
 				if(data.length > 0){
-					console.log(data);
+					console.log("has data: ", data);
 					sendMail(data)
 				} }
 	)
 	.catch(error => sendErrorMail(error))
 
-
-// console.log(today())
-// notifyIfAny()
 setInterval(notifyIfAny, process.env.FREQUENCY ||  300000);
+
+// keepFetching()
